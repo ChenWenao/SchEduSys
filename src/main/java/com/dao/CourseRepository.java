0805750247cc07793c20f5b66a13a32e985e8380
ChenWenao <747273894@qq.com>
2020-05-08 21:55:33 +0800

    bug 修复

--- src/main/java/com/dao/CourseRepository.java
+++ src/main/java/com/dao/CourseRepository.java
@@ -184,7 +184,9 @@ public class CourseRepository {
 
     public List<Course> selectAll(String param, String value) {
         try {
-            List<Course> courses = template.query("select * from Course where ? = ?", courseRowMapper, param, value);
+            String sql="select * from Course where "+param+" = "+value;
+            System.out.println(sql);
+            List<Course> courses = template.query(sql, courseRowMapper);
             return courses;
         } catch (Exception e) {
             System.out.println(e);
